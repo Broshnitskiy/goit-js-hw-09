44494549a18b30efb3a86500dea138f0ac7fdf9f
Roman Broshnitskiy <romario777v8@gmail.com>
2021-11-04 23:23:34 +0200

    Добавляє скрипт задачі №1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStartRef = document.querySelector("[data-start]");
+const btnStopRef = document.querySelector("[data-stop]");
 
+let intervalId = null;
+
+btnStartRef.addEventListener("click", (e) => {
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    e.target.setAttribute("disabled", "");
+});
+
+btnStopRef.addEventListener("click", () => {
+    clearInterval(intervalId);
+    btnStartRef.removeAttribute("disabled");
+});
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};

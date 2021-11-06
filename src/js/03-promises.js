48f259cb2d32ab4eff46e27f0a636d6291ef3806
Roman Broshnitskiy <romario777v8@gmail.com>
2021-11-06 18:20:16 +0200

    добавляє скрипт завдання №3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.1.0.min.css';
+
+const formRef = document.querySelector(".form");
+
+formRef.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const amountValue = e.target.elements.amount.value;
+  const delayValue = e.target.elements.delay.value;
+  const stepValue = e.target.elements.step.value;
+  let delay = delayValue;
+  
+  for (let i = 1; i <= amountValue; i += 1){
+    createPromise(i, delay, stepValue)
+    .then(({ position, delay }) => {
+        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+    // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+   
+    delay = Number(delay) + Number(stepValue);
+  };
+  
+})
+  
+  
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+    resolve({position, delay})
+      } else {
+    reject({position, delay})
+      }
+    }, delay)
+  })
 }
